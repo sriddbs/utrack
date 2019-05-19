bd2b7c6e19e2831df33b00c40887b2f61643f65d
Sridhar Belagod <sriddbs.bs@gmail.com>
2019-05-20 00:29:11 +0530

    Add specs for model validations

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:website_short_url) }
+    it { should validate_presence_of(:website_url) }
+  end
 end
