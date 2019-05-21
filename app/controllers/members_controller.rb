fcf8509acc92d91c631a79a9c73a601ee5ddc42b
Sridhar Belagod <sriddbs.bs@gmail.com>
2019-05-21 22:24:21 +0530

    Add pagination for members page

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,6 +1,6 @@
 class MembersController < ApplicationController
   def index
-    @members = Member.order('created_at DESC')
+    @members = Member.order('created_at DESC').paginate(page: params[:page])
   end
 
   def new
