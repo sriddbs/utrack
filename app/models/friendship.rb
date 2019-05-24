bf154a092b87c5cf6c680a4327c07e6edcc2452e
Sridhar Belagod <sriddbs.bs@gmail.com>
2019-05-24 23:10:03 +0530

    Display friends count in the profile page

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,6 +2,6 @@ class Friendship < ApplicationRecord
   validates :member_id, presence: true
   validates :friend_id, presence: true, uniqueness: { scope: :member_id }
 
-  belongs_to :member
-  belongs_to :friend, class_name: 'Member'
+  belongs_to :member, counter_cache: true
+  belongs_to :friend, class_name: 'Member', counter_cache: true
 end
